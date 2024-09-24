83551933ef983c113ffc9873e91a752236c24fd8
Giovanne Almeida <giovanne.almeida91@gmail.com>
2024-09-24 17:30:25 -0300

    Ajustes para aula 2 do dojo.
    - Faz request para puxar lista de ToDos.
    - Risca tarefas marcadas como concluídos.
    - Adiciona novas tarefas no fim da lista.

--- src/App.js
+++ src/App.js
@@ -1,56 +1,88 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
-    const [tarefas, setTarefas] = useState([]);
-    const [novaTarefa, setNovaTarefa] = useState('');
+  const [tarefas, setTarefas] = useState([]);
+  const [novaTarefa, setNovaTarefa] = useState('');
 
-    const addTarefa = () => {
-        setTarefas(tarefas.concat({id: Math.random(), descricao: novaTarefa, isChecked: false}));
-        setNovaTarefa('');
+  useEffect(() => {
+    const loadTodo = async () => {
+      console.log("fetching");
+      try {
+        const resposta = await fetch('https://jsonplaceholder.typicode.com/todos?userId=1');
+        const resultado = await resposta.json();
+        setTarefas(resultado.map(r => ({
+            id: r.id,
+            descricao: r.title,
+            isChecked: r.completed
+          })
+        ));
+      } catch (error) {
+        console.error('Erro ao carregar os dados:', error);
+      }
     };
 
-    const inputTarefa = (event) => {
-        setNovaTarefa(event.target.value);
-    };
+    loadTodo();
+  }, [])
 
-    const deleteTarefa = (id) => {
-        setTarefas(tarefas.filter(tarefa => tarefa.id !== id));
-    };
-
-    return (
-        <div className="App">
-          <h1>To-Do List</h1>
-          <div className="add-task-container">
-            <input
-              type="text"
-              value={novaTarefa}
-              onChange={inputTarefa}
-              placeholder="Nova tarefa"
-              className="task-input"
-            />
-            <button onClick={addTarefa} className="add-task-button">
-              Add tarefa
-            </button>
-          </div>
-          <ul>
-            {tarefas.map(tarefa => (
-              <li key={tarefa.id} className="task-item">
-                <input
-                  type="checkbox"
-                />
-                {tarefa.descricao}
-                <button
-                  onClick={() => deleteTarefa(tarefa.id)}
-                  className="delete-button"
-                >
-                  Excluir
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
+  const addTarefa = () => {
+    setTarefas(
+      [
+        { id: Math.random(), descricao: novaTarefa, isChecked: false }, ...tarefas
+      ]
     );
+    setNovaTarefa('');
+  };
+
+  const inputTarefa = (event) => {
+    setNovaTarefa(event.target.value);
+  };
+
+  const deleteTarefa = (id) => {
+    setTarefas(tarefas.filter(tarefa => tarefa.id !== id));
+  };
+
+  const completeTarefa = (id) => {
+    setTarefas(tarefas.map(tarefa => tarefa.id === id ? { ...tarefa, isChecked: !tarefa.isChecked } : tarefa));
+  };
+
+  return (
+    <div className="App">
+      <h1>To-Do List</h1>
+      <div className="add-task-container">
+        <input
+          type="text"
+          value={novaTarefa}
+          onChange={inputTarefa}
+          placeholder="Nova tarefa"
+          className="task-input"
+        />
+        <button onClick={addTarefa} className="add-task-button">
+          Add tarefa
+        </button>
+      </div>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa.id} className={`task-item ${tarefa.isChecked ? 'task-completed' : ''}`}>
+            <input
+              type="checkbox"
+              checked={tarefa.isChecked}
+              onChange={() => completeTarefa(tarefa.id)}
+            />
+            <span>
+              {tarefa.descricao}
+            </span>
+            <button
+              onClick={() => deleteTarefa(tarefa.id)}
+              className="delete-button"
+            >
+              Excluir
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
